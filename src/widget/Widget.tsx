6de12297978761881903b0f7cef07a32d6aca3c2
lapakota <stalkerzone955@gmail.com>
2022-03-27 00:52:15 +0500

    Add recent emojis logic and install prettier

--- src/widget/Widget.tsx
+++ src/widget/Widget.tsx
@@ -1,37 +1,60 @@
-import React, {useState} from "react";
-import EmojiGroup from "./EmojiGroup";
-import ChangeGroupButton from "./ChangeGroupButton";
-import "./Widget.scss";
+import React, { useState } from 'react';
+import EmojiGroup from './EmojiGroup';
+import ChangeGroupButton from './ChangeGroupButton';
+import './Widget.scss';
 import emojis from '../assets/emojis.json';
 
+const recentEmojisCount = 16;
+
 const Widget: React.FC = () => {
-    const emojiGroups = emojis.map(data => <EmojiGroup key={data.groupName} groupName={data.groupName}
-                                                       groupEmojis={data.groupEmojis}/>);
+    const [recentEmojis] = useState([] as EmojiType[]);
+
+    const updateRecentEmojis = (emoji: EmojiType) => {
+        const emojiIndex = recentEmojis.findIndex(e => e.char === emoji.char);
+        emojiIndex !== -1 && recentEmojis.splice(emojiIndex, 1);
+
+        recentEmojis.unshift(emoji);
+        recentEmojis.length > recentEmojisCount && recentEmojis.pop();
+    };
+
+    const emojiGroups = emojis.map(data => (
+        <EmojiGroup
+            key={data.groupName}
+            groupName={data.groupName}
+            groupEmojis={data.groupName !== 'Recent & Favourites' ? data.groupEmojis : recentEmojis}
+            updateRecent={updateRecentEmojis}
+        />
+    ));
+
     const [currentGroupIndex, setCurrentGroupIndex] = useState(1);
     const [currentGroup, setCurrentGroup] = useState(emojiGroups[currentGroupIndex]);
-
-    const icons = ['❤️', '😀', '🐹', '🍉', '🎃', '🌍', '🧻', '🉐'];
 
     const changeCurrentGroup = (index: number) => {
         setCurrentGroup(emojiGroups[index]);
         setCurrentGroupIndex(index);
-    }
+    };
+
+    const icons = ['❤️', '😀', '🐹', '🍉', '🎃', '🌍', '🧻', '🉐'];
 
     return (
         <div className="Widget">
             <div className={'buttons-wrapper'}>
-                {icons.map((icon, index) =>
-                    <ChangeGroupButton key={`ChangeGroupButton${index}`} idGroup={index} icon={icon}
-                                       onClick={() => changeCurrentGroup(index)}
-                                       isActive={index === currentGroupIndex}/>
-                )}
+                {icons.map((icon, index) => (
+                    <ChangeGroupButton
+                        key={`ChangeGroupButton${index}`}
+                        idGroup={index}
+                        icon={icon}
+                        onClick={() => changeCurrentGroup(index)}
+                        isActive={index === currentGroupIndex}
+                    />
+                ))}
             </div>
             <div className={'search'}>
-                <input className={'search-input'} placeholder={'Emoji Search'}/>
+                <input className={'search-input'} placeholder={'Emoji Search'} />
             </div>
             {currentGroup}
         </div>
     );
-}
+};
 
 export default Widget;
