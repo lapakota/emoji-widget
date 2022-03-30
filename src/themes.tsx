f37c05e420dda4d03dc410827a2036901cf006ef
PlyshkaLim <beauty-gzhill@mail.ru>
2022-03-30 17:34:02 +0500

    Added pretty toggle for change dark theme

--- src/themes.tsx
+++ src/themes.tsx
@@ -1,29 +1,29 @@
 export const light = {
-  body: {
-    backgroundColor: "#fff",
-  },
-  text: {
-    color: "#000",
-  },
-  button: {
-    color: 'black'
-  },
-  arrow: {
-    color: 'black',
-  }
+    body: {
+        backgroundColor: '#f5f5f5'
+    },
+    text: {
+        color: '#000'
+    },
+    button: {
+        color: 'black'
+    },
+    arrow: {
+        color: 'black'
+    }
 };
 
 export const dark = {
-  body: {
-    backgroundColor: "#222226",
-  },
-  text: {
-    color: "#fefefe",
-  },
-  button: {
-    color: 'red'
-  },
-  arrow: {
-    color: '#fefefe',
-  }
+    body: {
+        backgroundColor: '#222226'
+    },
+    text: {
+        color: '#fefefe'
+    },
+    button: {
+        color: 'red'
+    },
+    arrow: {
+        color: '#fefefe'
+    }
 };
