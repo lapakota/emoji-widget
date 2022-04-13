7b8e788f245de26474dbb8a010bba9df6079a742
lapakota <stalkerzone955@gmail.com>
2022-04-13 22:08:04 +0500

    fix insert emoji in textarea

--- public/content.js
+++ public/content.js
@@ -2,9 +2,9 @@ chrome.runtime.onMessage.addListener(function (request) {
     if (request.name === 'emoji') {
         const activeElement = document.activeElement;
 
-        if (activeElement instanceof HTMLInputElement) activeElement.value += request.char;
-        if (activeElement instanceof HTMLDivElement || activeElement instanceof HTMLTextAreaElement)
-            activeElement.textContent += request.char;
+        if (activeElement instanceof HTMLInputElement || activeElement instanceof HTMLTextAreaElement)
+            activeElement.value += request.char;
+        if (activeElement instanceof HTMLDivElement) activeElement.textContent += request.char;
 
         console.log(request.char, activeElement);
     }
