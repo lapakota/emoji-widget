4fe479724c1ee15828f4695041cb062694989b07
PlyshkaLim <beauty-gzhill@mail.ru>
2022-04-20 16:51:17 +0500

    add toggle border in focus

--- src/widget/WidgetGroups/SettingsGroup/SettingsGroup.tsx
+++ src/widget/WidgetGroups/SettingsGroup/SettingsGroup.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import './SettingsGroup.scss';
 import { CurrentThemeContext, ChangeThemeContext } from '../../../App';
+import cn from 'classnames';
 
 const SettingsGroup = () => {
     const isLightTheme = useContext(CurrentThemeContext);
@@ -14,7 +15,7 @@ const SettingsGroup = () => {
                         {dispatchChangeTheme => (
                             <input
                                 type={'checkbox'}
-                                className={'toggle'}
+                                className={cn('toggle', isLightTheme ? 'light-toggle' : 'dark-toggle')}
                                 onClick={() => dispatchChangeTheme(!isLightTheme)}
                                 defaultChecked={!isLightTheme}
                             />
